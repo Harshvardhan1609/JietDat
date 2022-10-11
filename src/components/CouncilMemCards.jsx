dea087fdca8dceacf76ed84187659593b7d3e4ac
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-10-11 20:21:43 +0530

    initial

--- src/components/CouncilMemCards.jsx
+++ src/components/CouncilMemCards.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 import './councilcards.css'
 
 export default function CouncilMemCards(props) {
@@ -8,6 +9,10 @@ export default function CouncilMemCards(props) {
     const btn = props.btn
     const linking = props.linking
 
+    function handleClick() {
+      <Navigate to="/Students" replace={true} />
+    }
+
   return (
    <div className="col-md-4 mb-4">
     <div class="card p-5 ">
@@ -15,7 +20,7 @@ export default function CouncilMemCards(props) {
   <div class="card-body text-center">
     <h5 class="card-title mb-3 fs-5 fw-bold">{head}</h5>
     <p class="card-text lead fs-6">{para}</p>
-    <a href={linking} target={"_blank"} rel="noreferrer" class="btn btn-primary rounded-pill">{btn}</a>
+    <a href={linking} target={"_blank"} rel="noreferrer" class="btn btn-primary rounded-pill" onClick={handleClick}>{btn}</a>
   </div>
 </div>
     </div>
