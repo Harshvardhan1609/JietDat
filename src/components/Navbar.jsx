d3dfd9050dd96e30542ad90d10e4d44ed3e08a29
HARSHVARDHAN PUROHIT <75427196+Harshvardhan1609@users.noreply.github.com>
2023-02-25 23:41:11 +0530

    ff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,14 +102,16 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../Navbar.css";
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { useNavigate } from 'react-router-dom';
 
 function NavBar() {
   const [click, setClick] = useState(false);
   
   const handleClick = () => setClick(!click);
-
+  const navigate = useNavigate();
   const goToContact = () => {
-    window.open('https://dailyprophets.netlify.app/', '_blank');
+//       navigate('/techtalks');
+     window.open('https://dailyprophets.netlify.app/', '_blank');
   }
 
   return (
@@ -176,21 +178,24 @@ function NavBar() {
                 Student Council
               </NavLink>
             </li>
-
-            <a href="https://dailyprophets.netlify.app/5" target={"_blank"} rel="noreferrer" class="btn btn-primary rounded-pill">Register Now</a>
-</li>
-          <button exact target={"_blank"} rel="noreferrer" class="btn btn-primary rounded-pill navib" onClick={goToContact}>
-            <i className='fa fa-user-plus me-2'></i>SOCIAL MEDIA 
-          </button>
-        </ul>
-      </div>
-      <div className="nav-icon" onClick={handleClick}>
-        <GiHamburgerMenu className="navi " value={{ color: 'white'}}/>
-        <i className={click ? "fas" : "fas"}></i>
-      </div>
-    </nav>
-  </>
-);
+            <li>
+              <a href="https://dailyprophets.netlify.app/5" target={"_blank"} rel="noreferrer" class="btn btn-primary rounded-pill">Register Now</a>
+            </li>
+            <li>
+              <button exact target={"_blank"} rel="noreferrer" class="btn btn-primary rounded-pill navib" onClick={goToContact}>
+                <i className='fa fa-user-plus me-2'></i>SOCIAL MEDIA 
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-icon" onClick={handleClick}>
+          <GiHamburgerMenu className="navi " value={{ color: 'white'}}/>
+          <i className={click ? "fas" : "fas"}></i>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
+
